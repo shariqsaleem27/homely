2c94261668f6d0369668a6335363924c0e545303
shariqsaleem27 <shariq.saleem06@gmail.com>
2024-09-21 14:48:50 +1000

    base backend

---
+++ backend/app.js
@@ -0,0 +1,36 @@
+// app.js
+const express = require('express');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+
+// Load environment variables from .env file
+dotenv.config();
+
+const app = express();
+
+// Middleware to parse incoming requests
+app.use(express.json());
+
+// Import Routes
+const   userRoutes = require('./routes/userRoutes');
+const restaurantRoutes = require('./routes/restaurantsRoutes');
+// const orderRoutes = require('./routes/orderRoutes');
+
+// Connect to MongoDB
+mongoose.connect(process.env.MONGO_URI, { 
+  useNewUrlParser: true, 
+  useUnifiedTopology: true 
+})
+.then(() => console.log('Connected to MongoDB'))
+.catch((err) => console.error('Could not connect to MongoDB', err));
+
+// Routes Middleware
+app.use('/api/users', userRoutes);
+app.use('/api/restaurants', restaurantRoutes);
+// app.use('/api/orders', orderRoutes);
+
+// Start server
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
